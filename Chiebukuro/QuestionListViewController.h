a2e91129ce145f31da9a8915dd70cfcca3c4c848
idekeita <k.ide@i.softbank.jp>
2014-09-07 14:59:04 +0900

    use ListViewController

---
+++ Chiebukuro/QuestionListViewController.h
@@ -0,0 +1,13 @@
+//
+//  QuestionListViewController.h
+//  Chiebukuro
+//
+//  Created by IDE KEITA on 2014/09/07.
+//  Copyright (c) 2014年 IDE KEITA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QuestionListViewController : UITableViewController
+
+@end
